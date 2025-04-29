9208c5a646c79acf1eae55acccdc6c7570b4f0c8
naimishvarsani <naimish587@gmail.com>
2025-04-29 16:56:43 +0530

    changes

--- lib/Screens/Intros/1.dart
+++ lib/Screens/Intros/1.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_interpolation_to_compose_strings
+// ignore_for_file: prefer_interpolation_to_compose_strings, deprecated_member_use
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
